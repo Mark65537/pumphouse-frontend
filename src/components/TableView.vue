936f5acff88d1eb4d817b755a3741fb536d7d061
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2023-12-23 00:37:45 +0700

    добавленна возможность изменять таблицу, пока что без отправки на Backend

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -10,8 +10,17 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="row in paginatedRows" :key="row.id">
-            <td v-for="(value, key) in row" :key="key">{{ value }}</td>
+          <tr v-for="row in paginatedRows" :key="row.num">
+            <td v-for="(value, key) in row" :key="key">
+              <input v-if="isEditable(row.num, key)" type="text" v-model="edits[row.num][key]" />
+              <span v-else @dblclick="enableEdit(row.num, key)" :class="{disabled: currentlyEditingRowNum !== null && currentlyEditingRowNum !== row.num}">
+                {{ value }}
+              </span>
+            </td>
+            <td>
+              <button v-if="isRowBeingEdited(row.num)" @click="saveEdits(row.num)">Сохранить</button>
+              <button v-if="isRowBeingEdited(row.num)" @click="cancelEdit(row.num)">Отменить</button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -42,15 +51,19 @@ export default {
     rows: {
       type: Array,
       default: () => [] // Теперь rows по умолчанию - пустой массив
-    }
+    },
   },
   data() {
     return {
       currentPage: 1,
       rowsPerPage: 5,
-      rowsPerPageOptions: [5, 10, 50, 100]
+      rowsPerPageOptions: [5, 10, 50, 100],
+
+      edits: {}, // Словарь для отслеживания изменений
+      currentlyEditingRowNum: null,
     };
   },
+  
   computed: {
     totalPages() {
       return Math.ceil(this.rows.length / this.rowsPerPage);
@@ -65,7 +78,56 @@ export default {
       return this.rows.slice(start, end);
     }
   },
+
   methods: {
+
+    isEditable(rowId, key) {
+      return this.edits[rowId] != null && 
+      Object.prototype.hasOwnProperty.call(this.edits[rowId], key);
+    },
+    isRowBeingEdited(rowId) {
+      console.log("isRowBeingEdited", rowId, this.currentlyEditingRowNum);
+      return this.currentlyEditingRowNum === rowId;
+    },
+    enableEdit(rowId, key) {
+      if (this.currentlyEditingRowNum === null || 
+              this.currentlyEditingRowNum === rowId) { // Если ни одна строка не редактируется или редактируется эта строка
+        
+        if (!this.edits[rowId]) {
+          this.edits[rowId] = {};
+        }
+        this.edits[rowId][key] = this.rows.find(row => row.num === rowId)[key];
+
+        this.currentlyEditingRowNum = rowId;
+
+      }
+      // console.log("enableEdit", rowId, key);
+    },
+    cancelEdit(rowId) {
+      if (this.edits[rowId]) {
+        delete this.edits[rowId];
+      }
+      this.currentlyEditingRowNum = null; // Сброс текущей редактируемой строки
+    },
+    async saveEdits(rowId) {
+      const updatedRow = this.edits[rowId];
+      try {
+        await this.updateRow(rowId, updatedRow); // Метод для отправки изменений на сервер
+        Object.assign(this.rows.find(row => row.num === rowId), updatedRow);
+        if (this.edits[rowId]) {
+          delete this.edits[rowId];
+        }
+      } catch (error) {
+        console.error('Ошибка при сохранении:', error);
+        // Обработка ошибок, возможно показ уведомления
+      }
+      this.currentlyEditingRowNum = null;
+    },
+    async updateRow(rowId, updatedRow) {
+      // Здесь должен быть код для отправки изменений на сервер
+      console.log('Отправка изменений на сервер:', rowId, updatedRow);
+    },
+
     nextPage() {
       if (this.currentPage < this.totalPages) {
         this.currentPage++;
@@ -139,6 +201,13 @@ th {
 
 .rounded-table th {
   border-top: 1px solid #ddd; /* Границы для заголовка таблицы */
+}
+
+/* Стили для компонента Disabled */
+.disabled {
+  color: #a1a1a1; /* светло-серый цвет текста */
+  cursor: not-allowed; /* курсор в виде знака запрета */
+  opacity: 0.6; /* полупрозрачность */
 }
 
 </style>
